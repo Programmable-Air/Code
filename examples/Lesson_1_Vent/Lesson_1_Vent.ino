a24945cb7a9fda08a75a11352de4791d5b479005
Amitabh Shrivastava <tinkrmind@gmail.com>
2023-02-15 00:01:54 -0500

    Curriculum Examples Added

---
+++ examples/Lesson_1_Vent/Lesson_1_Vent.ino
@@ -0,0 +1,27 @@
+// Programmable-Air
+// Author: tinkrmind
+// https://github.com/orgs/Programmable-Air
+// 
+// Basic example of using Vent function of the Programmable-Air board. 
+// The neopixels show pressure. Cyan for low pressure and Purple for high pressure
+// Brighter color means more extreme pressure. Bright cyan means lower pressure than dim cyan and brighter purple means higher pressure tham dim purple. At atmospheric presure,lights will be off.
+//
+// PCB v0.3/v0.4/v0.5
+
+#include "programmable_air.h"
+
+#define DEBUG 1
+
+void setup() {
+  initializePins();
+}
+
+void loop() {
+  // STEP 1 : Read pressure only. Valves and Pumps are all off. 
+  // You can turn the vent valve on and off manually using the yellow button.
+  // Using a syringe or your mouth try blowing or sucking on the output tube and see how the pressure and thus neopixels are affected.
+  delayWhileReadingPressure(200);
+
+  // STEP 2: Vent the output tube. See if you can affect the neopixels with the following line uncommented.
+  vent();
+}
---
+++ examples/Lesson_2_Blow/Lesson_2_Blow.ino
@@ -0,0 +1,28 @@
+// Programmable-Air
+// Author: tinkrmind
+// https://github.com/orgs/Programmable-Air
+// 
+// Basic example of using Blow function of the Programmable-Air board. 
+// The neopixels show pressure. Cyan for low pressure and Purple for high pressure
+// Brighter color means more extreme pressure. Bright cyan means lower pressure than dim cyan and brighter purple means higher pressure tham dim purple. At atmospheric presure,lights will be off.
+//
+// PCB v0.3/v0.4/v0.5
+
+#include "programmable_air.h"
+
+#define DEBUG 1
+
+void setup() {
+  initializePins();
+}
+
+void loop() {
+  delayWhileReadingPressure(200);
+
+  // if the RED button is pressed blow air into the output, else, vent to atmosphere
+  if(readBtn(RED)){
+    blow();
+  } else{
+    vent();
+  }
+}
